ef1ddc4fc2e367b497ad770b29558edb248b8ad6
phurinaix <phurinaix@gmail.com>
2018-11-22 07:22:49 +0700

    add die in connection

--- users/src/connection.php
+++ users/src/connection.php
@@ -1,2 +1,7 @@
 <?php
     $mysqli = new mysqli('localhost', 'root', '', 'users');
+    
+    // Check connection
+    if ($mysqli->connect_error) {
+        die("Connection failed: " . $mysqli->connect_error);
+    } 
